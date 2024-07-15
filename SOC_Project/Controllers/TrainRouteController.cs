bffbda8f688ba95f4f6d713767b5086a9cdd83a7
Aruna Shantha-Laptop <arunalk722@hotmail.com>
2024-07-15 20:48:12 +0530

    add sql query to insert

--- SOC_Project/Controllers/TrainRouteController.cs
+++ SOC_Project/Controllers/TrainRouteController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SOC_Project.FunctionFiles;
+using System.Data.SqlClient;
 using System.Diagnostics.Eventing.Reader;
 
 namespace SOC_Project.Controllers
@@ -11,15 +12,41 @@ namespace SOC_Project.Controllers
 
         [HttpPost]
         [Route("CreateTrain")]
-        public IActionResult CreateTrain(TrainRoute createLocation)
+        public IActionResult CreateTrainRoute(TrainRoute routeData)
         {
 
 
-            if (WebTokenValidate.TokenValidateing(createLocation.Token)) 
+            if (WebTokenValidate.TokenValidateing(routeData.Token)) 
             {
                 try
                 {
-                    string query = "";
+                    SqlParameter[] sqlParameters = new SqlParameter[]
+                    {
+                    new SqlParameter("@TrainID",routeData.TrainId),
+                    new SqlParameter("@SourLocation",routeData.SourLocatin),
+                    new SqlParameter("@DestLocation",routeData.DestLocation),
+                    new SqlParameter("@SchaduleTime",routeData.SchaduleTime),
+                    new SqlParameter("@CreatedUser",routeData.CreatedUser),
+                    new SqlParameter("@CreatedDate",DateTime.Now.ToString()),
+                    new SqlParameter("@IsActive",routeData.IsActive)
+                    };
+                    string query = "INSERT INTO [dbo].[tbl_TrainRoute] ([TrainID] ,[SourLocation] ,[DestLocation] ,[SchaduleTime] ,[CreatedUser] ,[CreatedDate] ,[IsActive]) VALUES (@TrainID, @SourLocation, @DestLocation, @SchaduleTime, @CreatedUser, @CreatedDate, @IsActive)";
+                    if (SQLConnection.PrmWrite(query, sqlParameters)) {
+                        return Ok(new StatusMessage
+                        {
+                            SCode = 200,
+                            SMessage="Train route Insert"
+                        });
+                    }
+                    else
+                    {
+                        return BadRequest(new StatusMessage
+                        {
+                            SCode = 500, // Or a more appropriate code based on the error
+                            SMessage = "An internal error occurred while creating the train route. Please check your input data."
+                        });
+                    }
+                   
                 }
                 catch(Exception ex)
                 {
